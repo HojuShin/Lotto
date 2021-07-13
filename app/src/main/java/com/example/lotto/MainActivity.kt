95e32dfa61a89e1b4549b3201f34ad0e231097df
HojuShin <shin03188@naver.com>
2021-07-14 07:05:17 +0900

    //

--- app/src/main/java/com/example/lotto/MainActivity.kt
+++ app/src/main/java/com/example/lotto/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.lotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<FloatingActionButton>(R.id.floatingActionButton).setOnClickListener {
+            Log.d("로또앱", "버튼 누름")
+        }
     }
 }
